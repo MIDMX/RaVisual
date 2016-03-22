892474962e6a391c977ba6ed272df65acf5a3a9e
MIDMX <time.kadel@sfr.fr>
2016-03-22 17:03:36 +0100

    same

--- appli/Trellis.c
+++ appli/Trellis.c
@@ -7,18 +7,20 @@
 #define HT16K33_BLINK_DISPLAYON 0x01
 #define HT16K33_CMD_BRIGHTNESS  0xE0
 
-/*
+
+
+
 void begin() {
 
   I2C1_init(); // initialize I2C peripheral
-  I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
+ // I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
   I2C_write(I2C1, 0x01); // write one byte to the slave
   I2C_stop(I2C1); // stop the transmission
   blinkRate(HT16K33_BLINK_OFF);
   
   setBrightness(15); // max brightness
 
-  I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
+ // I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
   I2C_write(I2C1, 0x01); // write one byte to the slave
   I2C_stop(I2C1); // stop the transmission
 
@@ -27,19 +29,19 @@ void begin() {
 
 void setLED(uint8_t x) {
   if (x > 15) return;
-  x = pgm_read_byte(&ledLUT[x]);
+ // x = pgm_read_byte(&ledLUT[x]);
   displaybuffer[x >> 4] |= _BV(x & 0x0F);
 }
 void clrLED(uint8_t x) {
   if (x > 15) return;
-  x = pgm_read_byte(&ledLUT[x]);
+//  x = pgm_read_byte(&ledLUT[x]);
   displaybuffer[x >> 4] &= ~_BV(x & 0x0F);
 }
 
 
 void setBrightness(uint8_t b) {
   if (b > 15) b = 15;
-  I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
+ // I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
   I2C_write(I2C1, 0x01); // write one byte to the slave
   I2C_stop(I2C1); // stop the transmission
   I2C_write(I2C1, HT16K33_CMD_BRIGHTNESS | b);
@@ -47,7 +49,7 @@ void setBrightness(uint8_t b) {
 }
 
 void blinkRate(uint8_t b) {
-  I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
+//  I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
   if (b > 3) b = 0; // turn off if not sure
   I2C_write(I2C1,HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (b << 1));
   I2C_stop(I2C1);
@@ -55,7 +57,7 @@ void blinkRate(uint8_t b) {
 
 
 void writeDisplay(void) {
-I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
+//I2C_start(I2C1, SLAVE_ADDRESS<<1, I2C_Direction_Transmitter);
 I2C_write(I2C1,(uint8_t)0x00); // start at address $00
 
   for (uint8_t i=0; i<8; i++) {
@@ -68,4 +70,4 @@ I2C_write(I2C1,(uint8_t)0x00); // start at address $00
 void clear(void) {
   memset(displaybuffer, 0, sizeof(displaybuffer));
 }
-*/
+
